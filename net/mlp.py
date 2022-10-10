9c223118c0f8863bee473cd4c7fce3f5cdfccc71
Kevin <Kevin Du>
2022-10-10 12:48:32 +0200

    Lint mlp.py

--- net/mlp.py
+++ net/mlp.py
@@ -2,6 +2,7 @@ from .linear import Linear
 from .network import Network
 from brunoflow.func.activations import relu
 
+
 class MLP(Network):
     def __init__(self, input_size=784, hidden_size=500, num_classes=10):
         self.ff1 = Linear(input_size, hidden_size)
