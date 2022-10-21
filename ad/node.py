1) Add recursive color naming for debugging, 2) Fix abs_val_grad computation (although linalg.inv hasn't implemented backward for abs_val_grad correctly, 3) Add some entropy tests (although entropy computation is broken for funcs that don't rely on pointwise-backward)

ad/node.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+from colorama import Style
+import graphviz
 import numpy as np
 import numpy.typing as npt
 from typing import Any, List, Tuple, Union
+
+# from brunoflow.ad import name
 
 
 class Node:
@@ -55,6 +59,7 @@ class Node:
         self.inputs = inputs
         self.name = name
         self.__num_uses = 0
+        self.graph = graphviz.Digraph("computation_graph", filename="computation_graph.gv")
 
     @property
     def shape(self):
@@ -107,8 +112,8 @@ class Node:
             self.entropy_wrt_output = 0.0
             self.abs_val_grad = 1.0
         if verbose:
+            print("Starting backprop:")
             print(
-                "Starting backprop:",
                 self.name,
                 ", num_uses:",
                 self.__num_uses,
@@ -131,14 +136,21 @@ class Node:
         Assumes that self.__compute_num_uses() has been called in advance
         """
         assert (self.max_grad_of_output_wrt_node[0] >= self.max_neg_grad_of_output_wrt_node[0]).all()
+        assert (self.abs_val_grad >= 0).all()
 
         if verbose:
             print(
-                f"\nCalling __backprop on node {self.name}",
+                f"{Style.RESET_ALL}\nCalling __backprop on node {self.name}",
                 ", num_uses:",
                 self.__num_uses,
                 ", max_grad:",
                 self.max_grad_of_output_wrt_node[0],
+                ", grad:",
+                self.grad,
+                ", abs_val_grad:",
+                self.abs_val_grad,
+                ", entropy_wrt_output:",
+                self.entropy_wrt_output,
             )
         # Record that backprop has reached this Node one more time
         self.__num_uses -= 1
@@ -161,10 +173,13 @@ class Node:
                 adjoints_max_neg_grad = backward_func(self.val, self.max_neg_grad_of_output_wrt_node[0], *input_vals)
 
                 # Required for running the entropy semiring properly, compute the abs value of the gradient
-                adjoints_abs_val_grad = (
-                    np.abs(adjoints)
-                    if isinstance(adjoints, (np.ndarray, np.floating, float))
-                    else tuple(np.abs(adjoint) if adjoint is not None else None for adjoint in adjoints)
+                adjoints_abs_val_grad = backward_func(
+                    self.val,
+                    dict(out_abs_val_grad=self.abs_val_grad),
+                    *input_vals,
+                    product_fn=(
+                        lambda l_adj, out_abs_val_grad_dict: np.abs(l_adj) * out_abs_val_grad_dict["out_abs_val_grad"]
+                    ),
                 )
 
                 # Semi-ring product (out_grad, -out_entropy) and (adj_grad, - adj_grad * log(adj_grad)) to get adjoint entropy (where adj_grad is the local derivative of self wrt each adjoint input)
@@ -178,7 +193,7 @@ class Node:
                         * out_grad_and_entropy_dict["out_entropy"]
                         + (-np.abs(l_adj) * np.log(np.abs(l_adj)) * out_grad_and_entropy_dict["out_abs_val_grad"])
                     ),
-                )
+                )  # TODO: this is broken for any functions that don't use pointwise_backward :(. Fix either in the hacky way used for abs_val_grad or make something more sustainable!
 
                 # print(self.max_grad_of_output_wrt_node[0])
                 assert (
@@ -195,7 +210,19 @@ class Node:
                     adj_max_neg_grad = adjoints_max_neg_grad[i]
                     adj_entropy = adjoints_entropy[i]
                     adj_abs_val_grad = adjoints_abs_val_grad[i]
+
                     if isinstance(self.inputs[i], Node):
+                        assert (
+                            self.inputs[i].grad.shape
+                            == self.inputs[i].max_grad_of_output_wrt_node[0].shape
+                            == self.inputs[i].max_neg_grad_of_output_wrt_node[0].shape
+                            == self.inputs[i].entropy_wrt_output.shape
+                            == self.inputs[i].abs_val_grad.shape
+                        )
+                        assert (
+                            adj_abs_val_grad >= 0
+                        ).all(), f"adj_abs_val_grad for output node w.r.t. input node {self.inputs[i]} via node {self} is {adj_abs_val_grad}. This number must be positive instead, so probably the backward func from {self} to {self.inputs[i]} is funky. Check the always-positive invariant holds!"
+
                         # An adjoint may need to be 'reshaped' before it can be accumulated if the forward operation used broadcasting.
                         adjoint_gradient = reshape_adjoint(adj, self.inputs[i].grad.shape)
                         adj_max_grad = reshape_adjoint(adj_max_grad, self.inputs[i].grad.shape)
@@ -204,7 +231,13 @@ class Node:
                         adj_abs_val_grad = reshape_adjoint(adj_abs_val_grad, self.inputs[i].grad.shape)
 
                         # ACCUMULATE GRADIENT BY SUMMING
+                        prev_grad = np.copy(self.inputs[i].grad)
                         self.inputs[i].grad += adjoint_gradient
+                        if verbose:
+                            print(
+                                f"    {Style.RESET_ALL}Updating grad of node {self.inputs[i].name}:\n     from {prev_grad} \n       to {self.inputs[i].grad}."
+                            )
+                        del prev_grad
 
                         # If the max pos grad is less than the max neg grad, that means there was some sign flipping, and we should reverse which is most positive and most negative. Use np.maximum and np.minimum to do this element-wise.
                         adj_max_grad, adj_max_neg_grad = np.maximum(adj_max_grad, adj_max_neg_grad), np.minimum(
@@ -227,6 +260,7 @@ class Node:
                             print(
                                 f"    Replacing max_grad of node {self.inputs[i].name}:\n     from {(prev_vals, prev_parents)} \n       to {self.inputs[i].max_grad_of_output_wrt_node}."
                             )
+                        del prev_vals, prev_parents
 
                         # ACCUMULATE MAX NEG GRAD BY USING THE MIN OPERATOR
                         # Replace the current (max neg grad value, parent) of the ith input node with (max neg grad value of self, self) for all elements in the ith input node that have more negative max neg grad values in self.
@@ -246,22 +280,28 @@ class Node:
                             print(
                                 f"    Replacing max_neg_grad of node {self.inputs[i].name}:\n     from {(prev_vals, prev_parents)} \n       to {self.inputs[i].max_neg_grad_of_output_wrt_node}."
                             )
+                        del prev_vals, prev_parents
+
+                        prev_abs_val_grad = np.copy(self.inputs[i].abs_val_grad)
+                        self.inputs[i].abs_val_grad += adj_abs_val_grad
+                        if verbose:
+                            print(
+                                f"    {Style.RESET_ALL}Updating abs_val_grad of node {self.inputs[i].name}:\n     from {prev_abs_val_grad} \n       to {self.inputs[i].abs_val_grad}."
+                            )
+                        del prev_abs_val_grad
 
                         # ACCUMULATE ENTROPY BY USING THE SUM OPERATOR
-                        prev_entropy = self.inputs[i].entropy_wrt_output
+                        prev_entropy = np.copy(self.inputs[i].entropy_wrt_output)
                         # print("adj_entropy:", adj_entropy)
                         self.inputs[i].entropy_wrt_output += adj_entropy
                         if verbose:
                             print(
-                                f"    Updating entropy of node {self.inputs[i].name}:\n     from {prev_entropy} \n       to {self.inputs[i].entropy_wrt_output}."
+                                f"    {Style.RESET_ALL}Updating unnormalized entropy of node {self.inputs[i].name}:\n     from {prev_entropy} \n       to {self.inputs[i].entropy_wrt_output}."
                             )
-
-                        prev_abs_val_grad = self.inputs[i].abs_val_grad
-                        self.inputs[i].abs_val_grad += adj_abs_val_grad
-                        if verbose:
                             print(
-                                f"    Updating abs_val_grad of node {self.inputs[i].name}:\n     from {prev_abs_val_grad} \n       to {self.inputs[i].abs_val_grad}."
+                                f"    {Style.RESET_ALL}Updating actual entropy of node {self.inputs[i].name}:\n     to {self.inputs[i].compute_entropy()}."
                             )
+                        del prev_entropy
 
             # Continue recursively backpropagating
             for inp in self.inputs:
@@ -297,6 +337,34 @@ class Node:
 
     def compute_entropy(self):
         return self.entropy_wrt_output / self.abs_val_grad + np.log(self.abs_val_grad)
+
+    def construct_graph(self):
+        # TODO: this simply doesn't work so if I want an actual graph visualizer I should fix this
+        inps = [self]
+        visited = set()
+        curr_node_name = self.name
+        self.graph.node(curr_node_name)
+        while inps:
+            curr_node = inps.pop()
+            curr_node_name = curr_node.name if isinstance(curr_node, Node) else str(curr_node)
+
+            if isinstance(curr_node, Node):
+                for inp in curr_node.inputs:
+                    print(inp)
+                    inp_name = inp.name if isinstance(inp, Node) else str(inp)
+                    inp_label = (
+                        inp_name.split(" ")[0][1:] if isinstance(inp, Node) and inp_name.startswith("(") else inp_name
+                    )
+                    if inp_name not in visited:
+                        self.graph.node(inp_name, label=inp_label)
+                        self.graph.edge(inp_name, curr_node_name)
+                        inps.append(inp)
+                        visited.add(inp_name)
+
+        return self.graph
+
+    def visualize(self):
+        self.graph.render(view=True)
 
 
 def reshape_adjoint(adj, shape):

func/linalg.py
@@ -22,9 +22,10 @@ def transpose(x, axes):
 
 
 def transpose_backward(out_val, out_grad, x, axes):
+    # This should be safe for abs_val_grad
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     inverse_perm = np.arange(len(axes))[np.argsort(axes)]
     return np.transpose(out_grad, inverse_perm), None
 
@@ -74,9 +75,19 @@ def diag(x, k=0):
 
 
 def diag_backward(out_val, out_grad, x, k):
+    """
+    This function is used to compute d(output)/d(input_to_diag), aka the gradient of the output node (e.g. loss) w.r.t. the input of the diag function.
+    It is NOT in the form of d(diag)/d(input_to_diag) * d(output)/d(diag) (aka local_gradient * out_grad) because of weird shape things?
+    We can compute it directly though, as shown here.
+
+    This function is also used to compute d_abs(output)/d_abs(input_to_diag) by the upstream function passing in
+        diag_node.abs_val_grad for out_grad (instead of diag_node.grad). This is safe here because, by inspection of the function, we have the invariant that
+        (diag_node.abs_val_grad >= 0) ==> (diag_backward(diag_node.abs_val_grad) >= 0).
+    """
+    # in this function, the value of out_grad may also doubles as the abs_val_grad too. Since abs_val_grad is enforced to be
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     if x.ndim == 2:
         return diagflat_nonsquare(out_grad, k, x.shape)
     else:
@@ -130,12 +141,23 @@ def det(x):
     return __det(x)
 
 
+# This function is used to compute d(output)/d(input_to_det), aka the gradient of the output node (e.g. loss) w.r.t. the input of the det function.
+# It is NOT in the form of d(det)/d(input_to_det) * d(output)/d(det) (aka local_gradient * out_grad) because of weird shape things?
+# We can compute it directly though, as shown here.
+
+# This function is also used to compute d_abs(output)/d_abs(input_to_det) by the upstream function passing in
+#     det_node.abs_val_grad for out_grad (instead of det_node.grad). This is safe here because, by inspection of the function, we have the invariant that
+#     (det_node.abs_val_grad >= 0) ==> (det_backward(det_node.abs_val_grad) >= 0).
+def det_backward(out_val, out_grad, x):
+    if isinstance(out_grad, dict):
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
+    return np.expand_dims(np.expand_dims(out_val * out_grad, -1), -1) * __np_matrix_transpose(np.linalg.inv(x))
+
+
 __det = make_function(
     lambda x: np.linalg.det(x),
-    lambda out_val, out_grad, x: np.expand_dims(
-        np.expand_dims(out_val * (out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"]), -1), -1
-    )
-    * __np_matrix_transpose(np.linalg.inv(x)),
+    det_backward,
     construct_single_variable_fct_name("det"),
 )
 
@@ -152,10 +174,21 @@ def inv(x):
 
 
 def inv_backward(out_val, out_grad, x):
-    out_val_T = __np_matrix_transpose(out_val)
-    return -np.matmul(
-        out_val_T, np.matmul(out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"], out_val_T)
+    """This might not be safe with entropy/abs val computations?"""
+    # TODO: fix this one!
+    print(
+        "WARNING: computing entropy and abs_val_grad of a graph containing this `bf.linalg.inv` function may "
+        "not be safe because the output of the backward pass may be negative, even if out_grad=inv_node.abs_val_grad is positive."
+        "If this node is required as part of a computation graph, then refactor code so that this computes "
+        "d_abs(output)/d_abs(input_to_inv) s.t. it MUST be non-negative."
     )
+    is_abs_val_grad = isinstance(out_grad, dict)
+    if is_abs_val_grad:
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
+
+    out_val_T = __np_matrix_transpose(out_val)
+    return -np.matmul(out_val_T, np.matmul(out_grad, out_val_T))
 
 
 __inv = make_function(lambda x: np.linalg.inv(x), inv_backward, construct_single_variable_fct_name("inv"))
@@ -199,12 +232,23 @@ def norm(x, axis=None):
 
 
 # Matrix multiplication and its infix operator (@)
+def matmul_backward(out_val, out_grad, A, B):
+    A_factor = np.copy(A)
+    B_factor = np.copy(B)
+    if isinstance(out_grad, dict):
+        out_grad = out_grad["out_abs_val_grad"]
+        A_factor = np.abs(A_factor)
+        B_factor = np.abs(B_factor)
+
+    return (
+        np.matmul(out_grad, __np_matrix_transpose(B_factor)),
+        np.matmul(__np_matrix_transpose(A_factor), out_grad),
+    )
+
+
 matmul = make_function(
     lambda A, B: np.matmul(A, B),
-    lambda out_val, out_grad, A, B: (
-        np.matmul(out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"], __np_matrix_transpose(B)),
-        np.matmul(__np_matrix_transpose(A), out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"]),
-    ),
+    matmul_backward,
     construct_double_variable_fct_name("matmul"),
 )
 Node.__matmul__ = matmul

func/reductions.py
@@ -32,8 +32,8 @@ def reduce_min(x, axis=None):
 
 def reduce_min_backward(out_val, out_grad, x, axis=None):
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     grad = np.zeros_like(x)
     if axis is None:
         min_index = np.argmin(x)
@@ -76,8 +76,8 @@ def reduce_max(x, axis=None):
 
 def reduce_max_backward(out_val, out_grad, x, axis=None):
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     grad = np.zeros_like(x)
     if axis is None:
         min_index = np.argmax(x)
@@ -120,8 +120,8 @@ def reduce_sum(x, axis=None):
 
 def reduce_sum_backward(out_val, out_grad, x, axis=None):
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     if axis is None:
         return np.full(x.shape, out_grad), None
     else:

func/shape.py
@@ -30,12 +30,19 @@ def reshape(x, newshape):
     return __reshape(x, newshape)
 
 
+def reshape_backward(out_val, out_grad, x, newshape):
+    if isinstance(out_grad, dict):
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
+    return (
+        np.reshape(out_grad, x.shape),
+        None,
+    )
+
+
 __reshape = make_function(
     np.reshape,
-    lambda out_val, out_grad, x, newshape: (
-        np.reshape(out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"], x.shape),
-        None,
-    ),  # if out_grad is a dict, then it contains the out_grad and out_entropy keys. Here, we only want out_grad
+    reshape_backward,
     construct_single_variable_fct_name("reshape", additional_arg_names=("shape",)),
 )
 
@@ -58,12 +65,19 @@ def squeeze(x, axis=None):
     return _squeeze(x, axis)
 
 
+def squeeze_backward(out_val, out_grad, x, axis):
+    if isinstance(out_grad, dict):
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
+    return (
+        np.reshape(out_grad, x.shape),
+        None,
+    )
+
+
 _squeeze = make_function(
     np.squeeze,
-    lambda out_val, out_grad, x, axis: (
-        np.reshape(out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"], x.shape),
-        None,
-    ),
+    squeeze_backward,
     construct_single_variable_fct_name("squeeze", additional_arg_names=("axis",)),
 )
 
@@ -86,12 +100,19 @@ def expand_dims(x, axis):
     return __expand_dims(x, axis)
 
 
+def expand_dims_backward(out_val, out_grad, x, axis):
+    if isinstance(out_grad, dict):
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
+    return (
+        np.reshape(out_grad, x.shape),
+        None,
+    )
+
+
 __expand_dims = make_function(
     np.expand_dims,
-    lambda out_val, out_grad, x, axis: (
-        np.reshape(out_grad if not isinstance(out_grad, dict) else out_grad["out_grad"], x.shape),
-        None,
-    ),
+    expand_dims_backward,
     construct_single_variable_fct_name("expand_dims", additional_arg_names=("axis",)),
 )
 
@@ -119,8 +140,8 @@ def concat(xs, axis=0):
 
 def concat_backward(out_val, out_grad, axis, *xs):
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     ret_grads = [None]  # no adjoint for the 'axis' argument
     start_idx = 0
     for x in xs:
@@ -208,8 +229,8 @@ def get_item(x, arg):
 
 def getitem_backward(out_val, out_grad, x, arg):
     if isinstance(out_grad, dict):
-        # value of out_grad is a dict containing keys out_grad and out_entropy
-        out_grad = out_grad["out_grad"]
+        # value of out_grad is a dict containing key out_abs_val_grad and maybe out_entropy
+        out_grad = out_grad["out_abs_val_grad"]
     grad = np.zeros_like(x)
     grad[arg] = out_grad
     return grad, None

test/utils.py
@@ -1,21 +1,26 @@
 from collections.abc import Iterable
+from typing import List
 import brunoflow as bf
 import numpy as np
 import random
 import torch
 from functools import reduce
 
+from brunoflow.ad.node import Node
+
 rtol = 1e-05
 atol = 1e-08
+
 
 class TestInput:
     def __init__(self, name, vals):
         self.name = name
         self.vals = vals
 
+
 def inputs_to_params(inp, lift_every_other=False):
     ret = []
-    for i,val in enumerate(inp.vals):
+    for i, val in enumerate(inp.vals):
         if isinstance(val, int) or (isinstance(val, np.ndarray) and val.dtype == int):
             ret.append(val)
         elif not lift_every_other or i % 2 == 0:
@@ -23,6 +28,7 @@ def inputs_to_params(inp, lift_every_other=False):
         else:
             ret.append(val)
     return ret
+
 
 def inputs_to_torch(inp, requires_grad=False):
     ret = []
@@ -36,6 +42,7 @@ def inputs_to_torch(inp, requires_grad=False):
         else:
             ret.append(torch.tensor(val, requires_grad=requires_grad))
     return ret
+
 
 def check(self, x, target):
     if isinstance(x, bf.ad.Node):
@@ -57,10 +64,12 @@ def check(self, x, target):
         for i in range(len(x)):
             check(self, x[i], target[i])
     else:
-        raise TypeError(f'Unknown type of x in check: {type(x)}')
+        raise TypeError(f"Unknown type of x in check: {type(x)}")
+
 
 def add_forward_tests(clss, basename, fn, torch_fn, inputs, lift_every_other=False):
     for inp in inputs:
+
         def test_forward(self):
             # print('======= FORWARD TEST =======')
             x = inputs_to_params(inp, lift_every_other)
@@ -72,10 +81,13 @@ def add_forward_tests(clss, basename, fn, torch_fn, inputs, lift_every_other=Fal
             # print('y:', y)
             # print('y_t:', y_t)
             check(self, y, y_t)
-        setattr(clss, f'test_forward_{basename}_{inp.name}', test_forward)
+
+        setattr(clss, f"test_forward_{basename}_{inp.name}", test_forward)
+
 
 def add_backward_tests(clss, basename, fn, torch_fn, inputs, lift_every_other=False):
     for inp in inputs:
+
         def test_backward(self):
             x = inputs_to_params(inp, lift_every_other)
             x_t = inputs_to_torch(inp, requires_grad=True)
@@ -95,25 +107,57 @@ def add_backward_tests(clss, basename, fn, torch_fn, inputs, lift_every_other=Fa
                 for x_ in x_t:
                     if x_.requires_grad:
                         x_.grad.zero_()
-        setattr(clss, f'test_backward_{basename}_{inp.name}', test_backward)
+
+        setattr(clss, f"test_backward_{basename}_{inp.name}", test_backward)
+
 
 def add_tests(clss, basename, fn, torch_fn, inputs, test_backward=True):
-    
+
     add_forward_tests(clss, basename, fn, torch_fn, inputs)
     if test_backward:
         add_backward_tests(clss, basename, fn, torch_fn, inputs)
     # Version where some inputs aren't lifted to be Parameters
     if len(inputs[0].vals) > 1:
-        add_forward_tests(clss, basename+'_halflifted', fn, torch_fn, inputs, lift_every_other=True)
+        add_forward_tests(clss, basename + "_halflifted", fn, torch_fn, inputs, lift_every_other=True)
         if test_backward:
-            add_backward_tests(clss, basename+'_halflifted', fn, torch_fn, inputs, lift_every_other=True)
+            add_backward_tests(clss, basename + "_halflifted", fn, torch_fn, inputs, lift_every_other=True)
+
 
 def inputs(input_vals):
-    return [TestInput(f'input{i}', vals) for i,vals in enumerate(input_vals)]
+    return [TestInput(f"input{i}", vals) for i, vals in enumerate(input_vals)]
+
 
 def random_inputs(input_shapes, rand=np.random.normal):
-    return [TestInput(\
-                reduce(lambda a,b: a + 'x' + b, [str(shape) for shape in shapes]),\
-                [rand(size=shape) for shape in shapes]\
-            )
-        for shapes in input_shapes]
+    return [
+        TestInput(
+            reduce(lambda a, b: a + "x" + b, [str(shape) for shape in shapes]), [rand(size=shape) for shape in shapes]
+        )
+        for shapes in input_shapes
+    ]
+
+
+def get_all_paths_to_node(curr_node: Node) -> List[List[Node]]:
+    # Helper for bruteforce computing backprop values
+    if not curr_node.inputs:
+        return [[curr_node]]
+    all_paths_to_curr_node = []
+    for inp in curr_node.inputs:
+        if isinstance(inp, Node):
+            paths_to_inp = get_all_paths_to_node(inp)
+            all_paths_to_curr_node_through_inp = [path + [curr_node] for path in paths_to_inp]
+            all_paths_to_curr_node += all_paths_to_curr_node_through_inp
+    return all_paths_to_curr_node
+
+
+def get_all_paths_from_src_to_target_node(target_node: Node, src_node: Node) -> List[List[Node]]:
+    # Helper for bruteforce computing backprop values
+    all_paths = get_all_paths_to_node(target_node)
+    all_paths_from_src_to_target = [
+        path for path in all_paths if np.all((path[0] == src_node).val)
+    ]  # Note - equality is simply checking if the value of the two nodes is the same, so be careful!
+    print(all_paths_from_src_to_target)
+    return all_paths_from_src_to_target
+
+
+def compute_path_values(paths: List[List[Node]]) -> List[float]:
+    pass