abbc10efa23d4ab798f10c7a7a0f88fc67f44c04
Kevin Du <kevin.du20@gmail.com>
2023-02-03 18:45:11 +0100

    Add dropout param to mlp

--- brunoflow/net/mlp.py
+++ brunoflow/net/mlp.py
@@ -1,4 +1,5 @@
 from .linear import Linear
+from .dropout import Dropout
 from .network import Network
 from brunoflow.func.activations import leakyrelu
 
@@ -6,13 +7,15 @@ from brunoflow.func.activations import leakyrelu
 class MLP(Network):
     typename = "MLP"
 
-    def __init__(self, input_size=784, hidden_size=500, num_classes=10):
+    def __init__(self, input_size=784, hidden_size=500, num_classes=10, dropout_prob=0.0):
         super(MLP, self).__init__()
         self.ff1 = Linear(input_size, hidden_size, name="ff1")
+        self.dropout = Dropout(p=dropout_prob)
         self.ff2 = Linear(hidden_size, num_classes, name="ff2")
 
     def forward(self, x):
         out = self.ff1(x)
         out = leakyrelu(out)
+        out = self.dropout(out)
         out = self.ff2(out)
         return out
